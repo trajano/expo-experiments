80c0b4935feee7f99556dbb158a426d052fab921
Archimedes Trajano <developer@trajano.net>
2024-08-20 21:34:46 -0400

    exclude dist folder

--- packages/my-text/src/index.ts
+++ packages/my-text/src/index.ts
@@ -3,6 +3,3 @@
  */
 export { useExpoGoogleFonts } from './useExpoGoogleFonts';
 export { MyText, MyTextE } from './MyText';
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
