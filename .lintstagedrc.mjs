d0bbaef4102b1d9218339639d3baaff340d7b952
Archimedes Trajano <developer@trajano.net>
2024-08-27 02:25:58 -0400

    ci: try to increment version

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,6 @@
 const baseConfig = {
   '*.{ts,tsx,js,mjs,json,md,yml,yaml}': 'prettier --write',
+  Dockerfile: 'prettier --write',
   // chances are JS files are used for configuration only, only TypeScript files would be considered part of the application
   '*.{js,mjs}': 'eslint --fix',
 };
