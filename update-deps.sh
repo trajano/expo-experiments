d293138ef5e570b9586e11d7bffbd6887489ccd9
Archimedes Trajano <developer@trajano.net>
2024-09-22 13:43:45 -0400

    chore: updated deps

---
+++ update-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -ex
+npx -y npm-check-updates -u
+pushd packages/my-app
+npx -y expo-doctor
+npx expo install --check
+popd
+
+# Commit only package.json and package-lock.json
+git add package.json **/package.json package-lock.json
+git commit -m "chore: updated deps"
