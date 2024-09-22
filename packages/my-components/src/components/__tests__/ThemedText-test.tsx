499cd347376d53597e744f916896383f093c1f65
Archimedes Trajano <developer@trajano.net>
2024-09-21 22:46:56 -0400

    refactor: move

--- packages/my-components/src/components/__tests__/ThemedText-test.tsx
+++ packages/my-components/src/components/__tests__/ThemedText-test.tsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react-native';
-import { ThemedText } from 'react-native-my-components/src/components/ThemedText';
+import { ThemedText } from '../ThemedText';
 
 it('renders correctly', () => {
   const { toJSON } = render(<ThemedText>Snapshot test!</ThemedText>);
