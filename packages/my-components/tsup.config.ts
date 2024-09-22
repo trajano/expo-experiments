24371c6ae7526586103d20ff5e781a8375d4a13d
Archimedes Trajano <developer@trajano.net>
2024-09-21 23:03:03 -0400

    fix: tsup configuration to support snap

--- packages/my-components/tsup.config.ts
+++ packages/my-components/tsup.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src', '!src/__tests__/**', '!src/**/*.test.*'],
   sourcemap: 'inline',
+  loader: {
+    '.snap': 'js',
+  },
 });
