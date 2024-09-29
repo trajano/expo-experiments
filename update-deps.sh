1917cf9a70d5e46ce2c42c071d685d92425d8b37
Archimedes Trajano <developer@trajano.net>
2024-09-29 00:24:47 -0400

    build(updatedeps): fix expo deps after update deps

--- update-deps.sh
+++ update-deps.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -ex
 npx -y npm-check-updates -u
+npm i
 pushd packages/my-app
+npx expo install --fix
 npx -y expo-doctor
-npx expo install --check
 popd
 
 # Commit only package.json and package-lock.json
 git add package.json **/package.json package-lock.json
-git commit -m "chore: updated deps"
+git commit -m "chore(deps): updated deps"
