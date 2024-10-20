774f97680ad39b592044f830f436b5061d1db195
Archimedes Trajano <developer@trajano.net>
2024-10-20 15:25:16 -0400

    test: verify android version computation

--- packages/my-app/__tests__/config.test.ts
+++ packages/my-app/__tests__/config.test.ts
@@ -1,6 +1,7 @@
-import { getConfig } from '@expo/config';
-import { dirname } from 'node:path';
+import { ExpoConfig, getConfig } from '@expo/config';
+import { dirname, join } from 'node:path';
 import appConfig from '../app.config';
+import { expo as expoConfig } from '../app.json';
 import metroConfig from '../metro.config';
 it('metro.config should load up properly', () => {
   expect(metroConfig).toBeTruthy();
@@ -9,11 +10,47 @@ it('metro.config should load up properly', () => {
 it('app.config should load up properly', () => {
   expect(appConfig).toBeTruthy();
   const config = getConfig(dirname(dirname(__filename)), {
-    isModdedConfig: true,
     isPublicConfig: true,
     strict: true,
   });
   const expoConfig = config.exp;
   expect(expoConfig).toBeTruthy();
   expect(expoConfig.android?.versionCode).toBe(102);
+  expect(expoConfig.version).toBe('1.2.3');
+  expect(config.staticConfigPath).toBe(
+    join(dirname(dirname(__filename)), 'app.json'),
+  );
+  expect(config.dynamicConfigPath).toBe(
+    join(dirname(dirname(__filename)), 'app.config.ts'),
+  );
+});
+
+describe('Versions match BUILD_BUILDNUMBER', () => {
+  let savedProcessEnv = process.env;
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...savedProcessEnv };
+  });
+  afterEach(() => {
+    process.env = savedProcessEnv;
+  });
+
+  const tryBuildNumber = (
+    buildNumber: string,
+    expectedAndroidVersionCode: number,
+  ): void => {
+    process.env.BUILD_BUILDNUMBER = buildNumber;
+    const config = appConfig({
+      config: expoConfig as Partial<ExpoConfig>,
+      projectRoot: dirname(dirname(__filename)),
+      staticConfigPath: join(dirname(dirname(__filename)), 'app.json'),
+      packageJsonPath: join(dirname(dirname(__filename)), 'package.json'),
+    });
+    return it(buildNumber, () => {
+      expect(config.android?.versionCode).toBe(expectedAndroidVersionCode);
+      expect(config.version).toBe(buildNumber);
+    });
+  };
+  tryBuildNumber('20241003.03', 2024100303);
+  tryBuildNumber('20241003.3', 2024100303);
 });
