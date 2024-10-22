1ed9877786c6019485def2e987803b8ce52f7303
Archimedes Trajano <developer@trajano.net>
2024-10-22 19:23:29 -0400

    feat(update): disable automatic updates
    Manually managed in the Updates storybook

--- packages/my-app/src/stories/Updates.stories.tsx
+++ packages/my-app/src/stories/Updates.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import * as Updates from 'expo-updates';
-import { FC } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { FC, useCallback } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
 import { MyText, Strong } from 'react-native-my-text';
 
 // List of enum objects to be excluded
@@ -23,11 +23,49 @@ const customReplacer = (key: string, value: any) => {
 const updateConstants = JSON.stringify(Updates, customReplacer, 2);
 const ExpoUpdatesView: FC = () => {
   const updatesState = Updates.useUpdates();
+  const checkForUpdates = useCallback(async () => {
+    await Updates.checkForUpdateAsync();
+  }, []);
+  const fetchUpdate = useCallback(async () => {
+    await Updates.fetchUpdateAsync();
+  }, []);
+  const reload = useCallback(async () => {
+    await Updates.reloadAsync();
+  }, []);
   return (
     <View style={styles.container}>
+      <MyText>
+        <Strong>Is update available</Strong>:{' '}
+        {updatesState.isUpdateAvailable ? 'Yes' : 'No'}
+      </MyText>
+      <MyText>
+        <Strong>Is update pending</Strong>:{' '}
+        {updatesState.isUpdatePending ? 'Yes' : 'No'}
+      </MyText>
+      <MyText>
+        <Strong>Is checking</Strong>: {updatesState.isChecking ? 'Yes' : 'No'}
+      </MyText>
+      <MyText>
+        <Strong>Is isDownloading</Strong>:{' '}
+        {updatesState.isDownloading ? 'Yes' : 'No'}
+      </MyText>
+      <Button
+        title="check for updates"
+        disabled={!Updates.isEnabled}
+        onPress={checkForUpdates}
+      />
+      <Button
+        title="fetch updates"
+        disabled={!Updates.isEnabled}
+        onPress={fetchUpdate}
+      />
+
+      <Button title="reload" disabled={!Updates.isEnabled} onPress={reload} />
+
       <MyText style={styles.text}>
         {JSON.stringify(updatesState, null, 2)}
       </MyText>
+
       <MyText style={styles.text}>
         <Strong>Constants</Strong>:
       </MyText>
