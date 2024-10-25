4f01adefac8daa2f4846bcbb3e964a43a11f70a3
Archimedes Trajano <developer@trajano.net>
2024-10-24 22:10:04 -0400

    ci: output files

--- eas-local-build.sh
+++ eas-local-build.sh
@@ -2,5 +2,5 @@
 set -ex
 npm i -g eas-cli
 cd packages/my-app
-eas build --platform=ios --profile=preview --local --non-interactive
-eas build --platform=ios --profile=development --local --non-interactive
+eas build --platform=ios --profile=preview --local --non-interactive --output=../../app.ipa
+eas build --platform=ios --profile=development --local --non-interactive --output=../../app-dev-client.ipa
