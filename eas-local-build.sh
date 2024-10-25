b0d9f01986d273c3271f71a302800251257db4bf
Archimedes Trajano <archimedes.trajano@iblum.co>
2024-10-24 20:01:09 -0400

    ci: fix

--- eas-local-build.sh
+++ eas-local-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -ex
 npm i -g eas-cli
+cd packages/my-app
 eas build --platform=ios --profile=preview --local --non-interactive
 eas build --platform=ios --profile=development --local --non-interactive
