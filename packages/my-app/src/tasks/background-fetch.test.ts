ed10dddbdb95a82a5cf9ca2aa916eaec22fe987f
Archimedes Trajano <developer@trajano.net>
2024-10-28 22:26:17 -0400

    test: background-fetch

---
+++ packages/my-app/src/tasks/background-fetch.test.ts
@@ -0,0 +1,36 @@
+import * as TaskManager from 'expo-task-manager';
+import { BACKGROUND_FETCH_TASK } from './background-fetch';
+import { BackgroundFetchResult } from 'expo-background-fetch';
+
+jest.mock('expo-task-manager', () => ({
+  ...jest.requireActual('expo-task-manager'),
+  defineTask: jest.fn(),
+}));
+jest.mock('@/logging', () => ({
+  backgroundFetchLog: {
+    log: jest.fn(),
+    debug: jest.fn(),
+  },
+}));
+
+describe('background-fetch', () => {
+  it('should have loaded', async () => {
+    expect(TaskManager.defineTask).toHaveBeenCalledWith(
+      BACKGROUND_FETCH_TASK,
+      expect.anything(),
+    );
+    const theFunction = jest.mocked(TaskManager.defineTask).mock
+      .lastCall![1] as (data: any) => Promise<BackgroundFetchResult>;
+
+    const result = await theFunction({
+      data: {},
+      error: null,
+      executionInfo: { taskName: 'myTask', eventId: 'eventId' },
+    });
+    expect(
+      result === BackgroundFetchResult.NewData ||
+        result === BackgroundFetchResult.NoData,
+    ).toBeTruthy();
+    expect(result === BackgroundFetchResult.Failed).toBeFalsy();
+  });
+});
