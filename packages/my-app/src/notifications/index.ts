433a8b90d75bd7ddcc6d78d8bbff12663bec8d23
Archimedes Trajano <developer@trajano.net>
2024-10-29 21:13:38 -0400

    docs: link to background notification information

--- packages/my-app/src/notifications/index.ts
+++ packages/my-app/src/notifications/index.ts
@@ -1,2 +1,6 @@
+/**
+ * Background notifications.
+ * https://docs.expo.dev/versions/latest/sdk/notifications/#background-notifications
+ */
 export { sharePushCurlCommandAsync } from './sharePushCurlCommandAsync';
 export { extractDataFromBackgroundNotification } from './extractDataFromBackgroundNotification';
