ea10cedb8c1194287c5a216c4831f8f29711bd33
Archimedes Trajano <archimedes.trajano@iblum.co>
2024-10-30 16:11:48 -0400

    build: local eas build should run npm i before hand and also rename output files

--- eas-local-build.sh
+++ eas-local-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -ex
 npm i -g eas-cli
+npm i
 cd packages/my-app
-eas build --platform=ios --profile=preview --local --non-interactive --output=../../app.ipa
-eas build --platform=ios --profile=development --local --non-interactive --output=../../app-dev-client.ipa
+eas build --platform=ios --profile=preview --local --non-interactive --output=../../my-app.ipa
+eas build --platform=ios --profile=development --local --non-interactive --output=../../my-app-dev-client.ipa
