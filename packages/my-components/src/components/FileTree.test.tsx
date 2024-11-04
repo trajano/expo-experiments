80a0ff0feadef7101bc90be0552867f027f92840
Archimedes Trajano <developer@trajano.net>
2024-11-04 00:24:15 -0500

    test: increased coverage

--- packages/my-components/src/components/FileTree.test.tsx
+++ packages/my-components/src/components/FileTree.test.tsx
@@ -4,8 +4,11 @@ import { FileTree } from './FileTree';
 import * as stories from './FileTree.stories';
 import { composeStories } from '@storybook/react';
 import FileViewer from 'react-native-file-viewer';
+import * as Haptics from 'expo-haptics';
+import { Alert } from 'react-native';
 
 jest.mock('expo-file-system');
+jest.mock('expo-haptics');
 jest.mock('react-native-file-viewer', () => ({ open: jest.fn() }));
 
 const { DocumentDirectory } = composeStories(stories);
@@ -189,5 +192,41 @@ describe('storybook of FileTree', () => {
     expect(treeItem).toBeTruthy();
     fireEvent.press(treeItem);
     expect(FileViewer.open).toHaveBeenCalledWith('mock//file.txt');
+  });
+
+  it('renders DocumentDirectory with one file to delete', async () => {
+    jest.spyOn(Alert, 'alert');
+    const mockFileSystem = FileSystem as jest.Mocked<typeof FileSystem>;
+    mockFileSystem.readDirectoryAsync.mockResolvedValue(['file.txt']);
+    mockFileSystem.getInfoAsync.mockResolvedValue({
+      isDirectory: false,
+      uri: 'foo://file.txt',
+      exists: true,
+      size: 42,
+      modificationTime: 1,
+    });
+    const { getByTestId } = render(<DocumentDirectory />);
+    await act(() => Promise.resolve());
+    const treeItem = getByTestId('touchable-tree-item');
+    expect(treeItem).toBeTruthy();
+    fireEvent(treeItem, 'longPress');
+    expect(Haptics.impactAsync).toHaveBeenCalledWith(
+      Haptics.ImpactFeedbackStyle.Heavy,
+    );
+    await act(() => Promise.resolve());
+    expect(Alert.alert).toHaveBeenCalledWith(
+      'Delete file',
+      'Do you want to delete file.txt?',
+      expect.anything(),
+    );
+    const yesButton = jest.mocked(Alert.alert).mock.calls[0][2][0];
+    expect(yesButton.text).toEqual('Yes');
+    await act(() => {
+      yesButton.onPress();
+    });
+
+    expect(FileSystem.deleteAsync).toHaveBeenCalledWith('mock//file.txt', {
+      idempotent: true,
+    });
   });
 });
