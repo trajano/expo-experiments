cf7e058ab70964e1221e3099bb70bb76c81e904d
Archimedes Trajano <developer@trajano.net>
2024-11-18 08:05:57 -0500

    refactor: use metro-config mergeConfig

--- packages/my-app/metro.config.js
+++ packages/my-app/metro.config.js
@@ -1,38 +1,38 @@
-// @ts-check
+// @ts-nocheck
 /* eslint-env node */
 const { getDefaultConfig } = require('expo/metro-config');
+const { mergeConfig } = require('metro-config');
 const withStorybook = require('@storybook/react-native/metro/withStorybook');
 const path = require('path');
 
 const monorepoRoot = path.resolve(__dirname, '../..');
 
-const config = getDefaultConfig(__dirname);
+let config = getDefaultConfig(__dirname);
 
-config.watchFolders = [monorepoRoot];
-config.resolver.nodeModulesPaths = [
-  ...config.resolver.nodeModulesPaths,
-  path.resolve(__dirname, 'node_modules'),
-  path.resolve(monorepoRoot, 'node_modules'),
-];
-const additionalAssetExts = ['lottie', 'fbx'];
-config.resolver.assetExts = [
-  ...config.resolver.assetExts,
-  ...additionalAssetExts,
-];
-const testRegexs = [/^.*\/[^/]+\.test\.[^/]+$/, /(\\__mocks__\\.*)$/];
-if (Array.isArray(config.resolver.blockList)) {
-config.resolver.blockList = [...config.resolver.blockList, ...testRegexs];
-} else if (typeof config.resolver.blockList === 'object') {
-  config.resolver.blockList = [config.resolver.blockList, ...testRegexs];
-}
-
-config.transformer.unstable_allowRequireContext = true;
-
-// due to the nature of this project where it's playing around with packages in and out it's best to reset the cache.
-config.resetCache = true;
-
-// @ts-ignore
-module.exports = withStorybook(config, {
+config = withStorybook(config, {
   enabled: true,
   configPath: path.resolve(__dirname, './.storybook'),
+});
+
+const testRegExps = [/^.*\/[^/]+\.test\.[^/]+$/, /(\\__mocks__\\.*)$/];
+/** @type {RegExp[]} */
+const blockList = Array.isArray(config.resolver.blockList)
+  ? [...config.resolver.blockList, ...testRegExps]
+  : [config.resolver.blockList, ...testRegExps];
+
+module.exports = mergeConfig(config, {
+  watchFolders: [monorepoRoot],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(monorepoRoot, 'node_modules'),
+    ],
+    assetExts: [...(config.resolver.assetExts ?? []), 'lottie', 'fbx'],
+    blockList,
+  },
+  transformer: {
+    unstable_allowRequireContext: true,
+  },
+  // due to the nature of this project where it's playing around with packages in and out it's best to reset the cache.
+  resetCache: true,
 });
