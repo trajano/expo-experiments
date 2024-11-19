875df1d7b0d34168749b647a2ad249a36c76fce0
Archimedes Trajano <developer@trajano.net>
2024-11-19 12:22:58 -0500

    feat: use react-native-pdf
    For now will use that one to render a PDF.

--- packages/my-app/src/app/secure/my-resume.tsx
+++ packages/my-app/src/app/secure/my-resume.tsx
@@ -1,17 +1,24 @@
-import { PdfView, PdfViewPortEventData } from 'react-native-pdf-view';
-import { FC, useCallback, useMemo, useRef, useState } from 'react';
-import { ImageStyle, Button, useWindowDimensions, View } from 'react-native';
-import PagerView from 'react-native-pager-view';
+import Pdf from 'react-native-pdf';
+import { FC, useCallback, useMemo, useState } from 'react';
+import { Button, ImageStyle, useWindowDimensions, View } from 'react-native';
+
+const samples = [
+  'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf',
+  'https://trajano.net/assets/Archimedes%20Trajano.pdf',
+  'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf',
+  'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+  'http://samples.leanpub.com/thereactnativebook-sample.pdf',
+];
 
 const ResumeScreen: FC = () => {
   const [uri, setUri] = useState(
     'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf',
   );
-  const [pageCount, setPageCount] = useState(1);
   const windowDimensions = useWindowDimensions();
-  const [viewport, setViewport] = useState<PdfViewPortEventData | null>(null);
-  const imageDimensions = useMemo<ImageStyle>(() => {
-    if (!viewport) {
+  const [viewWidth, setViewWidth] = useState(0);
+  const [viewHeight, setViewHeight] = useState(0);
+  const viewDimensions = useMemo<ImageStyle>(() => {
+    if (viewWidth === 0 || viewHeight === 0) {
       return {
         width: windowDimensions.width,
         height: 1,
@@ -19,63 +26,38 @@ const ResumeScreen: FC = () => {
     } else {
       return {
         width: windowDimensions.width,
-        height: (windowDimensions.width / viewport.width) * viewport.height,
-        // aspectRatio: viewport.width / viewport.height,
+        height: (windowDimensions.width / viewWidth) * viewHeight,
       };
     }
-  }, [windowDimensions.width, viewport]);
+  }, [windowDimensions.width, viewWidth, viewHeight]);
   const handleViewPortKnown = useCallback(
-    (nextViewport: PdfViewPortEventData) => {
-      console.log({ nextViewport });
-      setViewport(nextViewport);
+    (
+      _numberOfPages: number,
+      _path: string,
+      size: { height: number; width: number },
+    ) => {
+      setViewWidth(size.width);
+      setViewHeight(size.height);
     },
     [],
   );
-  const pagerViewRef = useRef<PagerView>(null);
   const flip = useCallback(() => {
-    const samples = [
-      'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf',
-      'https://trajano.net/assets/Archimedes%20Trajano.pdf',
-      'https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf',
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
-    ];
-
     const randomUri = samples[Math.floor(Math.random() * samples.length)];
     setUri(randomUri);
-    pagerViewRef.current?.setPage(0);
   }, []);
-  const pages = useMemo(
-    () =>
-      Array.from({ length: pageCount }, (_, index) => index + 1).map(
-        (pageNumber) => (
-          <View key={pageNumber}>
-            <PdfView
-              uri={uri}
-              cachePolicy={'disk'}
-              pageNumber={pageNumber}
-              onError={(error) => {
-                console.error(error);
-              }}
-              onViewPortKnown={handleViewPortKnown}
-              onPageCountKnown={(pageCountData) =>
-                setPageCount(pageCountData.pageCount)
-              }
-              style={imageDimensions}
-            />
-            <Button onPress={flip} title={uri} />
-          </View>
-        ),
-      ),
-    [uri, handleViewPortKnown, imageDimensions, pageCount, flip],
-  );
+
   return (
-    <PagerView
-      initialPage={0}
-      ref={pagerViewRef}
-      style={{ flex: 1, backgroundColor: 'beige' }}
-    >
-      {pages}
-    </PagerView>
+    <View>
+      <Pdf
+        source={{ uri }}
+        onLoadComplete={handleViewPortKnown}
+        onError={(error) => {
+          console.error(error);
+        }}
+        style={viewDimensions}
+      />
+      <Button title={uri} onPress={flip} />
+    </View>
   );
 };
 
