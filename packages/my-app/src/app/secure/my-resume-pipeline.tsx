eac32b258f0f2bbdc07dec033b6a0c47c7736a83
Archimedes Trajano <developer@trajano.net>
2024-11-27 02:17:20 -0500

    fix: pdf pipeline view works better now

--- packages/my-app/src/app/secure/my-resume-pipeline.tsx
+++ packages/my-app/src/app/secure/my-resume-pipeline.tsx
@@ -1,6 +1,6 @@
 import { PdfPipelineView } from 'react-native-pdf-view';
 import { FC, useCallback, useState } from 'react';
-import { Button, ScrollView, StyleSheet } from 'react-native';
+import { Button, ScrollView, StyleSheet, View } from 'react-native';
 import samples from '@/data/samplePdfUrls.json';
 import {
   usePdfPipeline,
@@ -10,6 +10,8 @@ import * as Crypto from 'expo-crypto';
 import { Asset } from 'expo-asset';
 import { useRouter } from 'expo-router';
 import { useViewDimensions } from '@/hooks/useViewDimensions';
+import PagerView from 'react-native-pager-view';
+import { MyText } from 'react-native-my-text';
 
 const ResumeScreen: FC = () => {
   const [uri, setUri] = useState(samples[0]);
@@ -34,31 +36,52 @@ const ResumeScreen: FC = () => {
   }, [loadedUri]);
   const router = useRouter();
   console.debug({ uri, pageCount, dimensions });
-  const pageNumber = 1;
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <PdfPipelineView
-        style={[styles.pdf, dimensions]}
-        uri={uri}
-        pageNumber={pageNumber}
-        onPageCountKnown={(e) => {
-          console.info(e);
-          if (pageNumber === 1) {
-            setPageCount(e.pageCount);
-          }
-        }}
-        onViewPortKnown={(e) => {
-          setWidth(e.width);
-          setHeight(e.height);
-        }}
-        onError={(e) => {
-          console.error(e);
-        }}
-        onLoad={() => {
-          setLoadedUri(uri);
-          setLoading(false);
-        }}
-      />
+      <PagerView
+        style={{ backgroundColor: 'yellow', ...dimensions }}
+        scrollEnabled={true}
+      >
+        {Array.from({ length: pageCount }, (_ignore, index) => index + 1).map(
+          (pageNumber) => (
+            <View
+              key={`${pageNumber}`}
+              style={{
+                backgroundColor: 'purple',
+                width: '100%',
+                height: '100%',
+              }}
+            >
+              <View style={{ alignSelf: 'center' }}>
+                <MyText>{pageNumber}</MyText>
+              </View>
+              <PdfPipelineView
+                style={[styles.pdf, dimensions]}
+                uri={uri}
+                pageNumber={pageNumber}
+                onPageCountKnown={(e) => {
+                  if (pageNumber === 1) {
+                    setPageCount(e.pageCount);
+                  }
+                }}
+                onViewPortKnown={(e) => {
+                  if (pageNumber === 1) {
+                    setWidth(e.width);
+                    setHeight(e.height);
+                  }
+                }}
+                onError={(e) => {
+                  console.error(e);
+                }}
+                onLoad={() => {
+                  setLoadedUri(uri);
+                  setLoading(false);
+                }}
+              />
+            </View>
+          ),
+        )}
+      </PagerView>
       <Button title={loadedUri} onPress={flip} disabled={loading} />
       <Button title={uri} onPress={post} />
       <Button
