70fa0ce7146437f1f54ebc4e846e6f69b544a125
Archimedes Trajano <developer@trajano.net>
2024-11-29 00:04:50 -0500

    test: coverage

--- packages/react-native-ipc-web/src/IpcWeb.test.tsx
+++ packages/react-native-ipc-web/src/IpcWeb.test.tsx
@@ -1,6 +1,6 @@
 import { act, fireEvent, render, screen } from '@testing-library/react-native';
 import { Button } from 'react-native';
-import { IpcWebConsumer, IpcWebProvider } from './index';
+import { IpcWebConsumer, IpcWebProvider, useIpcWeb, WithIpcWeb } from './index';
 import { simpleEchoServer } from './simpleEchoServer';
 
 describe('IpcWeb', () => {
@@ -22,6 +22,32 @@ describe('IpcWeb', () => {
           }}
         </IpcWebConsumer>
       </IpcWebProvider>,
+    );
+    await act(() => Promise.resolve());
+    fireEvent.press(screen.getByText('send'));
+  }, 30_000);
+
+  it('Use HoC', async () => {
+    const onMessage = jest.fn();
+    const TestComponent = () => {
+      const { IpcWebView, postMessage } = useIpcWeb();
+      return (
+        <>
+          <IpcWebView />
+          <Button
+            onPress={() => postMessage({ input: 'foo', for: 'xyz' })}
+            title="send"
+          />
+        </>
+      );
+    };
+    const CompositeComponent = WithIpcWeb(TestComponent);
+
+    render(
+      <CompositeComponent
+        sourceProvider={simpleEchoServer}
+        onMessage={onMessage}
+      />,
     );
     await act(() => Promise.resolve());
     fireEvent.press(screen.getByText('send'));
